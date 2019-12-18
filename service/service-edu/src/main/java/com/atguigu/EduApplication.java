5a6cde4d63603a9e6d9fd6dd31896006a165d8eb
lichao <lc_xywhy@163.com>
2019-12-18 20:59:33 +0800

    Initial commit

---
+++ service/service-edu/src/main/java/com/atguigu/EduApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ * @create 2019-12-18 20:39
+ */
+@SpringBootApplication
+public class EduApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(EduApplication.class,args);
+    }
+
+
+
+}
